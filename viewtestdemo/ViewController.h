9bb184d9c8966425a5a740eec722d7d431b59bb2
张 磊 <maxzhang1985@zhangmatoMac.local>
2012-07-23 11:19:19 +0800

    Initial Commit

---
+++ viewtestdemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  viewtestdemo
+//
+//  Created by 磊 张 on 12-7-23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ viewtestdemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  viewtestdemo
+//
+//  Created by 磊 张 on 12-7-23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
